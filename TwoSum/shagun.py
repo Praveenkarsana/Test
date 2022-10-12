9a0ae71db826f48950753622df77670f251d284c
shagun1023 <shaguns5275@gmail.com>
2022-10-12 22:15:32 +0530

    two sum

--- TwoSum/shagun.py
+++ TwoSum/shagun.py
@@ -1,3 +1,12 @@
-a=7
-b=2
-print("The sum is :" ,a+b)
+n = [2, 7, 11, 15]
+t = 26
+c = 0
+def sum():
+    for x in n:
+        for y in n:
+            if x+y==t:
+                # c = x+y
+                print(n.index(x),n.index(y))
+                return c
+                
+sum()
