75f28e68deb1d13801c63ae3dbbcdbd561acfb55
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-15 23:32:52 +0530

    demonstration of input method

---
+++ TEST/rudram.py
@@ -0,0 +1,7 @@
+#demonstration of input 
+name=input("Enter student name")
+roll=int(input("Enter roll number of student"))
+percentage=float(input("Enter student score in %"))
+print("name",name) 
+print("Roll number",roll)
+print("score",percentage)
