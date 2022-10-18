89aeeb21d2c1feb010cc649a519e5d15a3d1986c
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-18 22:16:20 +0530

    sum average values

---
+++ TEST/totalsumavg.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+num3=int(input("Enter third number :"))
+
+total=num1+num2+num3
+avg=total/3
+print("total",total)
+print("average is", avg)
