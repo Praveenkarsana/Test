ae2c8dd026ee36a97c5550cddda42f87f54d4a3b
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-25 22:19:25 +0530

    greater of three

---
+++ TEST/elif1.py
@@ -0,0 +1,10 @@
+a=int(input('Enter a value'))
+b=int(input('Enter b value'))
+c=int(input('Enter c value'))
+
+if a>b & a>c:
+    print('a is greater')
+elif b>c & b>a:
+    print('b is greater')
+else:
+   print('c is greater')        
