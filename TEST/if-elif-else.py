7a72f85e7dd5f2b4c4b062afbc62bc6a26d2633e
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-26 20:28:41 +0530

    biggest of three value from command line

---
+++ TEST/if-elif-else.py
@@ -0,0 +1,9 @@
+a=int(input('Enter a value'))
+b=int(input('Enter b value'))
+c=int(input('Enter c value'))
+if a>b & a>c:
+    print('A is greater than B & C')
+elif b>c:
+    print('B is greater')
+else:
+    print('C is greater')    
