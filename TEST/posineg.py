bbb9e2434c19d118093bc79e8f74a1d506c4a642
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-26 19:50:42 +0530

    to check a number is positive or negative

---
+++ TEST/posineg.py
@@ -0,0 +1,11 @@
+x=input("Enter number")
+if int(x)>0:
+    print('+ve value')
+    print('First if block statement')
+    print('outside of the first if block')
+
+x=input('Enter any number')
+if int(x)<0:
+    print('-ve value')
+    print('Second if blo4ck statement')
+    print('outside of the second if block')
