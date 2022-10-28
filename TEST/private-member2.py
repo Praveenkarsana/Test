e501a3b92ee39cc1c8f051b162677d5f242256db
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-28 22:45:50 +0530

    Private member encapsulation

---
+++ TEST/private-member2.py
@@ -0,0 +1,12 @@
+class NorthEast:
+    __length=0
+    __breadth=0
+    def __init__(self):
+        self.__length=5
+        self.__breadth=6
+        print(self.__length)
+        print(self.__breadth)
+
+NE=NorthEast()
+print(NE.length)
+print(NE.length)        
